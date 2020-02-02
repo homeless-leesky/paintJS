a3cacb199f992093471c22aabb42ba6ce6b8b323
이하늘 <hee1149kr@naver.com>
2020-02-02 12:23:13 +0900

    Add function-> Resizing brush size, Save painting, Filling canvas with color

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range =  document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c"
+const CANVAS_SIZE = 700;
+const CURSOR_STYLE= "css-cursor";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle =  INITIAL_COLOR;
+
 ctx.lineWidth = 2.5;
 
 let painting = false;
-
+let filling = false;
 
 function onMouseMove(event){
     const x=  event.offsetX;
@@ -37,17 +48,74 @@ function stopPainting(event){
 }
 
 
-function onMousedown(event){
-    //console.log(event)
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+
+}
+function handleRangeChange(event){
+    const brushSize = event.target.value;
+    ctx.lineWidth = brushSize;
+}
+function handleModeClick(event){
+    if(filling === true){
+        filling = false;
+        canvas.classList.remove(CURSOR_STYLE)
+        mode.innerText ="Fill";
+    }
+    else{
+        filling = true;
+        canvas.classList.add(CURSOR_STYLE)
+        mode.innerText ="paint"
+    }
+}
+
+function handleCanvasCilck(event){
+    if(filling){
+        ctx.fillRect(0,0, canvas.width, canvas.height);
+    }
+}
+
+function handleCm(event){
+    event.preventDefault();
 }
 
 
-
+function handleSaveClick(event){
+    const img = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = img;
+    link.download = "PaintJS[EXPORT]😈";
+    link.click();
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click",handleCanvasCilck);
+    canvas.addEventListener("contextmenu", handleCm);
+}
+
+
+// Array.from(colors).forEach(function(v){
+//     v.addEventListener("click",handleColorClick);    
+// })
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick
+    )
+);
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click",handleModeClick);
+}
+if(save){
+    save.addEventListener("click", handleSaveClick);
 }
